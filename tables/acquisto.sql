977da3f29f442cfb1ca49e4dca681a8de2b1809b
Riccardo De Zen <56487105+RicDeZen@users.noreply.github.com>
2020-03-05 18:25:02 +0100

    Update acquisto.sql

--- tables/acquisto.sql
+++ tables/acquisto.sql
@@ -2,8 +2,8 @@ CREATE TABLE acquisto
 (
     numero SERIAL,
     dataacquisto date NOT NULL,
-    dipendente fiscal_code COLLATE NOT NULL,
-    cliente fiscal_code COLLATE NOT NULL,
+    dipendente fiscal_code NOT NULL,
+    cliente fiscal_code NOT NULL,
     CONSTRAINT acquisto_pkey PRIMARY KEY (numero),
     CONSTRAINT acquisto_cliente_fkey FOREIGN KEY (cliente)
         REFERENCES cliente (cf) MATCH SIMPLE
