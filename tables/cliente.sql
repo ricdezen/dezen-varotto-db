74819a27e31e771d0550ad44f2e04a6da7a37cc9
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2020-04-04 15:09:49 +0200

    Integer anzichè cf in cliente

--- tables/cliente.sql
+++ tables/cliente.sql
@@ -1,11 +1,11 @@
 CREATE TABLE cliente
 (
-    cf tipo_codice_fiscale NOT NULL,
+    id SERIAL,
     nome character varying(20) NOT NULL,
     cognome character varying(20) NOT NULL,
     data_nascita date NOT NULL,
     telefono tipo_numero_telefono,
     email tipo_email,
-    CONSTRAINT cliente_pkey PRIMARY KEY (cf),
+    CONSTRAINT cliente_pkey PRIMARY KEY (id),
     CONSTRAINT informazione_di_contatto CHECK (telefono IS NOT NULL OR email IS NOT NULL)
 );
