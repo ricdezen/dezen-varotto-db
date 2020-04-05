97183a64bb64f32a9d11ca460bd36935f156379f
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2020-04-05 20:04:29 +0200

    Renamed fornitore to distributore

--- tables/catalogo.sql
+++ tables/catalogo.sql
@@ -1,11 +1,11 @@
 CREATE TABLE catalogo
 (
-    fornitore tipo_partita_iva NOT NULL,
+    distributore tipo_partita_iva NOT NULL,
     libro tipo_codice_isbn NOT NULL,
     prezzo numeric(11,2) NOT NULL,
-    CONSTRAINT catalogo_pkey PRIMARY KEY (fornitore, libro),
-    CONSTRAINT catalogo_fornitore_fkey FOREIGN KEY (fornitore)
-        REFERENCES fornitore (piva) MATCH SIMPLE
+    CONSTRAINT catalogo_pkey PRIMARY KEY (distributore, libro),
+    CONSTRAINT catalogo_distributore_fkey FOREIGN KEY (distributore)
+        REFERENCES distributore (piva) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE RESTRICT,
     CONSTRAINT catalogo_libro_fkey FOREIGN KEY (libro)
