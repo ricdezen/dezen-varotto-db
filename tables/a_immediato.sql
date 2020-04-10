e3e51fcb3acaa955fce3f8fd8fd0f1cf346a2f20
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2020-04-10 21:50:35 +0200

    Diviso acquisto nelle sue generalizzazioni

---
+++ tables/a_immediato.sql
@@ -0,0 +1,13 @@
+CREATE TABLE a_immediato (
+    numero integer NOT NULL,
+    cliente integer,
+    CONSTRAINT immediato_pkey PRIMARY KEY (numero),
+    CONSTRAINT immediato_fkey_acquisto FOREIGN KEY (numero)
+        REFERENCES acquisto (numero) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION,
+    CONSTRAINT prenotazione_fkey_cliente FOREIGN KEY (cliente)
+        REFERENCES cliente (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION
+);
--- tables/prenotazione.sql
+++ tables/prenotazione.sql
@@ -1,8 +1,17 @@
 CREATE TABLE prenotazione
 (
-    numero SERIAL,
+    numero integer NOT NULL,
     stato stato_prenotazione NOT NULL,
+    cliente integer NOT NULL,
     data_ritiro date,
     CONSTRAINT prenotazione_pkey PRIMARY KEY (numero),
+    CONSTRAINT prenotazione_fkey_acquisto FOREIGN KEY (numero)
+        REFERENCES acquisto (numero) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION,
+    CONSTRAINT prenotazione_fkey_cliente FOREIGN KEY (cliente)
+        REFERENCES cliente (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION,
     CONSTRAINT prenotazione_data_se_chiusa CHECK (stato != 'Ritirato' OR data_ritiro IS NOT NULL)
 );
