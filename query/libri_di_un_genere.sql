bf46d4ee82bb3f192da972ad37b29ba5cbf3fb4d
Riccardo De Zen <56487105+RicDeZen@users.noreply.github.com>
2020-04-12 00:33:40 +0200

    Update libri_di_un_genere.sql

--- query/libri_di_un_genere.sql
+++ query/libri_di_un_genere.sql
@@ -1,5 +1,5 @@
 /*
-QUERY 8
+QUERY 6
 Il codice ISBN, titolo e nome della collana di appartenenza di tutti i libri che appartengono a un certo genere.
 */
 SELECT DISTINCT L.isbn AS "ISBN", L.titolo AS "Titolo", L.nome_collana AS "Collana"
