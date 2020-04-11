fb8f95b85b68a71bd276ccda47a78db8e8a58dec
Riccardo De Zen <56487105+RicDeZen@users.noreply.github.com>
2020-04-12 00:27:12 +0200

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -1,5 +1,5 @@
 /*
-  Query 1
+	Query 1
 */
 CREATE FUNCTION dipendente_piu_vendite ()
 RETURNS TABLE(
@@ -15,7 +15,40 @@ RETURNS TABLE(
 $$ LANGUAGE SQL;
 
 /*
-  Query 4
+	Query 2
+*/
+CREATE FUNCTION fornitore_miglior_prezzo (book tipo_codice_isbn)
+RETURNS TABLE("Fornitore" tipo_partita_iva, "Prezzo" numeric) AS $$
+	SELECT fornitore AS "Fornitore", prezzo AS "Prezzo"
+	FROM catalogo
+	WHERE libro = book
+	AND prezzo IN (
+    	SELECT MIN(prezzo)
+    	FROM catalogo
+    	WHERE libro = book
+	);
+$$ LANGUAGE SQL;
+
+/*
+	Query 3
+*/
+CREATE FUNCTION genere_piu_venduto ()
+RETURNS TABLE(
+	"Genere più venduto" character varying,
+	"Descrizione" text,
+	"Vendite" bigint
+) AS $$
+    SELECT G.nome AS "Genere più venduto", G.descrizione AS "Descrizione", V.quantita AS "Vendite"
+	FROM vendite_per_genere AS V
+	JOIN genere AS G
+	ON G.nome = V.genere
+	WHERE V.quantita IN (
+    	SELECT MAX(quantita) FROM vendite_per_genere
+	);
+$$ LANGUAGE SQL;
+
+/*
+	Query 4
 */
 CREATE FUNCTION case_editrici_vendite (giorno date)
 RETURNS TABLE("Casa Editrice" character varying(20)) AS $$
@@ -29,12 +62,28 @@ RETURNS TABLE("Casa Editrice" character varying(20)) AS $$
 $$ LANGUAGE SQL;
 
 /*
-  Query 7
+	Query 7
 */
 CREATE FUNCTION bilancio () RETURNS numeric(11,2) AS $$
-    SELECT
+	SELECT
 		(SELECT SUM(importo) FROM acquisto) -
 		(SELECT SUM(importo) FROM ordine) -
 		(SELECT SUM(stipendio) FROM dipendente)
-	  AS "Bilancio";
+	AS "Bilancio";
+$$ LANGUAGE SQL;
+
+/*
+	Query 8
+*/
+CREATE FUNCTION libri_genere (book tipo_codice_isbn)
+RETURNS TABLE(
+	"ISBN" tipo_codice_isbn,
+	"Titolo" character varying,
+	"Collana" character varying
+) AS $$
+    SELECT DISTINCT L.isbn AS "ISBN", L.titolo AS "Titolo", L.nome_collana AS "Collana"
+	FROM appartiene AS A
+	JOIN libro AS L
+	ON A.libro = L.isbn
+	WHERE A.genere = book;
 $$ LANGUAGE SQL;
