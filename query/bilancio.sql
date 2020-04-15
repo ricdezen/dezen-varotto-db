cfe67b6a69c4c8da9d973805b49124de68a9bd67
Riccardo De Zen <56487105+RicDeZen@users.noreply.github.com>
2020-04-15 11:31:40 +0200

    Update bilancio.sql

--- query/bilancio.sql
+++ query/bilancio.sql
@@ -4,7 +4,7 @@ Ottenere il bilancio economico di un certo anno, inteso come il totale vendite d
 meno i costi di approvvigionamento di libri, meno gli stipendi degli impiegati.
 */
 SELECT
-(SELECT SUM(importo) FROM acquisto) -
-(SELECT SUM(importo) FROM ordine) -
+(SELECT SUM(importo) FROM acquisto WHERE date_part('year', data_acquisto) = XXXX) -
+(SELECT SUM(importo) FROM ordine WHERE date_part('year', data_ordine) = XXXX) -
 (SELECT SUM(stipendio) FROM dipendente)
 AS "Bilancio";
